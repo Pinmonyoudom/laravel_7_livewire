788119dafa96ecb2885acb9b6478800ac69c51fa
pinmonyoudom <pinmonyoudom718@gmail.com>
2021-03-12 14:00:33 +0700

    search & pagination

--- app/Http/Livewire/SearchContacts.php
+++ app/Http/Livewire/SearchContacts.php
@@ -3,27 +3,32 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use Illuminate\Pagination\Paginator;
 use App\Contact;
 
 class SearchContacts extends Component
 {
+    use WithPagination;
+
     public $search;
-
-    protected $updatesQueryString = [
-        'search' => ['except' => '']
-    ];
-
-    public function mount()
-    {
-        $this->search = request()->query('search', $this->search);
-    }
-
+    public $currentPage = 1;
+    
     public function render()
-    {
-        return view('livewire.search-contacts', [
-            'contacts' => Contact::where('name', 'like', '%'.$this->search.'%')
-            ->orWhere('phone_number','like','%'.$this->search.'%')
-            ->get(),
+    {   
+        return view('livewire.search-contacts',[
+            'contacts'      =>  Contact::where(function($sub_query){
+                                $sub_query->where('name','like','%'.$this->search.'%')
+                                          ->orWhere('phone_number','like','%'.$this->search.'%');
+            })->paginate(10)
         ]);
+    }
+    public function setPage($url)
+    {
+        //this SetPage method will accept the request for load next page data
+        $this->currentPage = explode('page=',$url)[1];//will make array from url and return page number, after getting page number
+        Paginator::currentPageResolver(function(){
+            return $this->currentPage;
+        });//this method will set the current page resolver
     }
 }
--- resources/views/livewire/search-contacts.blade.php
+++ resources/views/livewire/search-contacts.blade.php
@@ -1,31 +1,30 @@
-<div class="container mx-auto mt-12">
-    <h3 class="mb-12 text-center text-gray-900 text-4xl font-semibold">Livewire search</h3>
-
-    <!-- Searchbox -->
-    <div class="w-full p-6 mb-6 bg-white border rounded shadow-xl">
-        <input
-            wire:model="search"
-            type="text"
-            placeholder="Search..."
-            class="form-control"
-        >
-    </div>
-    <!-- // Searchbox -->
-
-    <!-- Search Results -->
-    <div class="w-full p-6 divide-y bg-white border rounded shadow-xl">
-        @foreach($contacts as $contact)
-            <div class="p-2">
-                <h5 class="text-md font-semibold text-gray-900">{{ $contact->name }}</h5>
-                <p class="text-gray-900">
-                    {{ $contact->phone_number }}
-                </p>
-            </div>
-        @endforeach
-
-        @if($contacts == '[]')
-            <p class="text-gray-900">No contacts found...</p>
-        @endif
-    </div>
-    <!-- // Search Results -->
+<div>
+    <div class="container">
+	    <div class="row">
+	        <div class="col-md-12">	            
+	            <input type="text"  class="form-control" placeholder="Search" wire:model="search" />
+	            <table class="table table-bordered" style="margin: 10px 0 10px 0;">
+	                <tr>
+	                    <th>Name</th>
+	                    <th>Phone Number</th>
+	                </tr>
+	                @foreach($contacts as $contact)
+	                <tr>
+	                    <td>
+	                        {{ $contact->name }}
+	                    </td>
+	                    <td>
+	                        {{ $contact->phone_number }}
+	                    </td>
+	                </tr>
+	                @endforeach
+	            </table>
+	            {{ $contacts->links('livewire.pagination') }}
+	        </div>
+	    </div>
+	</div>
 </div>
+
+
+
+
